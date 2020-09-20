39287fd0f0ea6b304f19c77d8434583c34ccd8d7
Aayushi <moonshot.iyer@gmail.com>
2020-09-20 19:05:21 +0530

    Updates

---
+++ precache-manifest.d3d7dfd535f30ef73ccedc02118e31b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72a67f3a83aaef4018e99ca03b53f6d",
+    "url": "/moonshot-aayushi/NASA-Apod.git/index.html"
+  },
+  {
+    "revision": "31e5fdb3d68dd9d838d0",
+    "url": "/moonshot-aayushi/NASA-Apod.git/static/css/main.5bb65448.chunk.css"
+  },
+  {
+    "revision": "fc7986ed740cc2ceae1a",
+    "url": "/moonshot-aayushi/NASA-Apod.git/static/js/2.6d0840d6.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/moonshot-aayushi/NASA-Apod.git/static/js/2.6d0840d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e5fdb3d68dd9d838d0",
+    "url": "/moonshot-aayushi/NASA-Apod.git/static/js/main.984aa528.chunk.js"
+  },
+  {
+    "revision": "9604b51844b1f9907d94",
+    "url": "/moonshot-aayushi/NASA-Apod.git/static/js/runtime-main.ab773fc1.js"
+  }
+]);
